73aff58d8966a99803b1846c56cd8c58bb001a66
nekoimi <nekoimime@gmail.com>
2022-04-29 09:49:01 +0800

    modify db migration script :card_file_box:

--- src/main/resources/migrations/v1/v1.0__create_table.sql
+++ src/main/resources/migrations/v1/v1.0__create_table.sql
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS sys_file_info
     relative_path varchar(255)            null default null
 );
 
-COMMENT ON TABLE sys_file_info IS '文件信息表';
+COMMENT ON TABLE sys_file_info IS '文件信息';
 COMMENT ON COLUMN sys_file_info.id IS '主键';
 COMMENT ON COLUMN sys_file_info.created_at IS '创建时间';
 COMMENT ON COLUMN sys_file_info.updated_at IS '最后更新时间';
@@ -24,7 +24,7 @@ COMMENT ON COLUMN sys_file_info.relative_path IS '相对路径';
 
 
 
-CREATE TABLE IF NOT EXISTS "user"
+CREATE TABLE IF NOT EXISTS sys_user
 (
     id         varchar(32) primary key not null,
     created_at timestamp               null,
@@ -36,12 +36,34 @@ CREATE TABLE IF NOT EXISTS "user"
     enable     smallint                null default 1
 );
 
-COMMENT ON TABLE "user" IS '文件信息表';
-COMMENT ON COLUMN "user".id IS '主键';
-COMMENT ON COLUMN "user".created_at IS '创建时间';
-COMMENT ON COLUMN "user".updated_at IS '最后更新时间';
-COMMENT ON COLUMN "user".deleted IS '软删除字段；1 - 已删除，0 - 未删除';
+COMMENT ON TABLE sys_user IS '账号信息';
+COMMENT ON COLUMN sys_user.id IS '主键';
+COMMENT ON COLUMN sys_user.created_at IS '创建时间';
+COMMENT ON COLUMN sys_user.updated_at IS '最后更新时间';
+COMMENT ON COLUMN sys_user.deleted IS '软删除字段；1 - 已删除，0 - 未删除';
 
-COMMENT ON COLUMN "user".username IS '登录账号，各种奇奇怪怪的账号';
-COMMENT ON COLUMN "user".password IS '密码';
-COMMENT ON COLUMN "user".enable IS '是否被禁用：0 - 禁用，1 - 正常';
+COMMENT ON COLUMN sys_user.username IS '账号';
+COMMENT ON COLUMN sys_user.password IS '密码';
+COMMENT ON COLUMN sys_user.enable IS '是否被禁用：0 - 禁用，1 - 正常';
+
+
+
+CREATE TABLE IF NOT EXISTS sys_user_info
+(
+    id         varchar(32) primary key not null,
+    created_at timestamp               null,
+    updated_at timestamp               null,
+    deleted    smallint                null,
+
+    nickname   varchar(255)            null default null,
+    avatar     varchar(255)            null default null
+);
+
+COMMENT ON TABLE sys_user_info IS '用户信息';
+COMMENT ON COLUMN sys_user_info.id IS '主键';
+COMMENT ON COLUMN sys_user_info.created_at IS '创建时间';
+COMMENT ON COLUMN sys_user_info.updated_at IS '最后更新时间';
+COMMENT ON COLUMN sys_user_info.deleted IS '软删除字段；1 - 已删除，0 - 未删除';
+
+COMMENT ON COLUMN sys_user_info.nickname IS '昵称';
+COMMENT ON COLUMN sys_user_info.avatar IS '头像';
